473862c4ac584a681188d65a1183ef17045b47b7
Jim Velasco <jim.velasco@gmail.com>
2018-06-02 12:00:40 -0600

    initial load

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -8,5 +12,40 @@ const router = express.Router();
 router.get("/test", (req, res) => res.json({ msg: "users works" }));
 
 router.get("/", (req, res) => res.json({ msg: "gets all users" }));
+
+// @route   GET api/users/register
+// @desc    Tests users route
+// @access  Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already esists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
