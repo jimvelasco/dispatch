9f14c37ba8503f830053effc74b13c21023eb30a
Jim Velasco <jim.velasco@gmail.com>
2018-06-05 17:20:01 -0600

    new key for mlab dispatch db

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
-  mongoURI:
-    "mongodb://jimvelasco:jimvelasc0@ds061188.mlab.com:61188/devconnect",
+  // mongoURI: "mongodb://jimvelasco:jimvelasc0@ds061188.mlab.com:61188/devconnect",
+  mongoURI: "mongodb://jimvelasco:jimvelasc0@ds147890.mlab.com:47890/dispatch",
+
   // "mongodb://localhost:27017/test",
   secretOrKey: "secret"
 };
