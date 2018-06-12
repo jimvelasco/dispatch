ea16442fa49b18dd23c146efbf7349a9fde5dc1d
tg970 <tgoodman@outlook.com>
2018-06-12 15:35:38 -0500

    esLint installed, running, and a few rules added

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaVersion": 6,
+    },
+    "rules": {
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "arrow-body-style": ["error", "always"],
+    },
+};
